83cc2ef07742deee584cdfaa1d37bc5da581d1e9
Kostya Zhdanov <pinchv@gmail.com>
2022-03-01 15:32:22 +0300

    [SNF-XXX] Add code for non interger coefficient

--- src/pageContent/index.js
+++ src/pageContent/index.js
@@ -1,7 +1,11 @@
 import { getSignNowIframe, getContent } from './config';
 import textNodes from './textNodes';
 
-const toLengthenValuesSimple = (factor) => (value) => {
+const getDecimalFactor = (sizeFactor) => {
+  return Number((sizeFactor - Math.floor(sizeFactor)).toFixed(1));
+};
+
+const toLengthenValuesInteger = (factor) => (value) => {
   let result = value;
   let coef = Number(factor);
 
@@ -13,6 +17,32 @@ const toLengthenValuesSimple = (factor) => (value) => {
   return result;
 };
 
+const toLengthenValuesDecimal = (factor) => (value) => {
+  let decimalPart = getDecimalFactor(factor);
+  let decimalResult = '';
+  let prevPosition = 0;
+  const value01Length = value.length * .1;
+
+  while(decimalPart > 0) {
+    decimalResult = decimalResult + value.slice(prevPosition, prevPosition + value01Length);
+    decimalPart -= .1;
+    prevPosition = prevPosition + value01Length;
+  }
+
+  return decimalResult;
+}
+
+const toLengthenValues = (factor) => (value) => {
+  const integerFactorText = toLengthenValuesInteger(Math.floor(factor))(value);
+  const decimalPart = getDecimalFactor(factor);
+
+  if (decimalPart !== 0) {
+    return `${integerFactorText}${toLengthenValuesDecimal(factor)(value)}`
+  }
+
+  return integerFactorText;
+}
+
 async function Start() {
   const snfFrame = getSignNowIframe();
 
@@ -20,7 +50,7 @@ async function Start() {
     const nodes = textNodes.getAll(getContent(snfFrame));
     const { sizeFactor } = await chrome.storage.sync.get(['sizeFactor']);
 
-    textNodes.updateAll(toLengthenValuesSimple(sizeFactor))(nodes);
+    textNodes.updateAll(toLengthenValues(sizeFactor))(nodes);
   } else {
     alert('SignNow Iframe didn\'t find');
   }
